e9acf911612fb830bdf5fe5bd35e70b4506a3a5f
Justin Jia <justin.jia.developer@gmail.com>
2016-02-27 02:27:29 -0800

    Add testing code

--- Handshake/Handshake/MainViewController.swift
+++ Handshake/Handshake/MainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PGMappingKit
 
 class MainViewController: UIViewController, ShakeHandlerDelegate {
 
@@ -14,8 +15,13 @@ class MainViewController: UIViewController, ShakeHandlerDelegate {
         super.viewDidLoad()
 
         /* Usage: Initlize ShakeHandler with self as delegate (we added ShakeHandlerDelegate above). Then call sendPerson when needed. */
-//        let shakeHandler = ShakeHandler(delegate: self)
-//        shakeHandler.sendPerson(<#T##person: Person##Person#>, inside: <#T##NSManagedObjectContext#>)
+
+        let shakeHandler = ShakeHandler(delegate: self)
+
+        let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let person = delegate.managedObjectContext.save(["firstName": "Justin", "lastName": "Jia", "email": "justin.jia@icloud.com"], description: shakeHandler.mapping, error: nil) as! Person
+
+        shakeHandler.sendPerson(person, inside: delegate.managedObjectContext)
     }
 
     // MARK: ShakeHandlerDelegate
