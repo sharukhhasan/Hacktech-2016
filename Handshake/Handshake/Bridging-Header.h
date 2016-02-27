97e2fa26dfc0240d032992c24988dff9b516c79f
Marion X <marionxu1@gmail.com>
2016-02-27 00:39:18 -0800

    initial fbsdk stuff/bridging header

---
+++ Handshake/Handshake/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Handshake
+//
+//  Created by Marion Xu on 2/27/16.
+//  Copyright © 2016 TintPoint. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_Header_h */
