de5d473cf501d5794ab0aebbdc7f7b8582ffcab9
Marion X <marionxu1@gmail.com>
2016-02-27 01:24:45 -0800

    welcome view controller

---
+++ Handshake/Handshake/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  Handshake
+//
+//  Created by Marion Xu on 2/27/16.
+//  Copyright © 2016 TintPoint. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
