a0080c3a19a18a5ffdf9db80d560975e8f81555f
Marion X <marionxu1@gmail.com>
2016-02-27 04:11:51 -0800

    swiping works yay

--- Handshake/Handshake/WelcomeViewController.swift
+++ Handshake/Handshake/WelcomeViewController.swift
@@ -17,6 +17,31 @@ class WelcomeViewController: UIViewController {
         
         //TODO: Set text to be name of logged in user from FB
         welcomeName.text = "Welcome, " + "{{Name}}" + "!"
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+    }
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                print("Swiped right")
+                performSegueWithIdentifier("shakeToTableRight", sender: nil)
+                break
+            case UISwipeGestureRecognizerDirection.Left:
+                print("Swiped left")
+                performSegueWithIdentifier("shakeToTableLeft", sender: nil)
+                break
+            default:
+                break
+            }
+        }
     }
     
 }
