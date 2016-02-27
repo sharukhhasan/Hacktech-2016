38fc872535e86a3a2ce67b31365c4aee4784671c
Marion X <marionxu1@gmail.com>
2016-02-27 02:17:42 -0800

    past handshake view

---
+++ Handshake/Handshake/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewController.swift
+//  Handshake
+//
+//  Created by Marion Xu on 2/27/16.
+//  Copyright © 2016 TintPoint. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
