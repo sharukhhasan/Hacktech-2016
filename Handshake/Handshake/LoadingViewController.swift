210a84b51a019398536c9d982df57aad6d8ce2c0
Marion X <marionxu1@gmail.com>
2016-02-27 01:48:43 -0800

    loading view controller

---
+++ Handshake/Handshake/LoadingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingViewController.swift
+//  Handshake
+//
+//  Created by Marion Xu on 2/27/16.
+//  Copyright © 2016 TintPoint. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
