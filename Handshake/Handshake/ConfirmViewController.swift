6b03c8ff03a31033967783ae7ab624b31fe1efff
Marion X <marionxu1@gmail.com>
2016-02-27 02:12:16 -0800

    confirm view

---
+++ Handshake/Handshake/ConfirmViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConfirmViewController.swift
+//  Handshake
+//
+//  Created by Marion Xu on 2/27/16.
+//  Copyright © 2016 TintPoint. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
