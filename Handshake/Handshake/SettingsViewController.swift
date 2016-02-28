641c65e4a7335cb3ecbf671f6e8ada102c3a871d
Justin Jia <justin.jia.developer@gmail.com>
2016-02-27 21:17:56 -0800

    Bug fixes

--- Handshake/Handshake/SettingsViewController.swift
+++ Handshake/Handshake/SettingsViewController.swift
@@ -71,6 +71,5 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        performSegueWithIdentifier("DetailedViewSegue", sender: self)
     }
 }
